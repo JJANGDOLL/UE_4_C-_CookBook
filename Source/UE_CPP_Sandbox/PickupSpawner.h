e9f864c20a2615aacb97a2ac3844c5490e24a71a
Kim Jae Jun <jjkim092@gmail.com>
2021-07-16 03:05:03 +0900

    Unreal RespawnItem
    https://megaton.synology.me/?p=859

---
+++ Source/UE_CPP_Sandbox/PickupSpawner.h
@@ -0,0 +1,42 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "PickupSpawner.generated.h"
+
+class APickUp;
+
+UCLASS()
+class UE_CPP_SANDBOX_API APickupSpawner : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	APickupSpawner();
+
+    UPROPERTY()
+    USceneComponent* SpawnLocation;
+
+    UFUNCTION()
+    void PickupCollected();
+
+    UFUNCTION()
+    void SpawnPickup();
+
+    UPROPERTY()
+    APickUp* CurrentPickup;
+
+    FTimerHandle MyTimer;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
