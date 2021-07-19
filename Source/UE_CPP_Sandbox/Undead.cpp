dbeb2caa6bb9c94291f20fbe0f7d5a162184fd52
Kim Jae Jun <jjkim092@gmail.com>
2021-07-19 10:04:13 +0900

    Unreal UInterface Inheritance
    https://megaton.synology.me/?p=933

---
+++ Source/UE_CPP_Sandbox/Undead.cpp
@@ -0,0 +1,31 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Undead.h"
+
+#include "Engine/Engine.h"
+
+// Add default functionality here for any IUndead functions that are not pure virtual.
+bool IUndead::IsDead()
+{
+    return  true;
+}
+
+void IUndead::Die()
+{
+    GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Red, TEXT("You can't kill what is already dead.  Mwahaha"));
+}
+
+void IUndead::Turn()
+{
+    GEngine->AddOnScreenDebugMessage(-1, 1, FColor::Red, TEXT("I'm Feeling"));
+}
+
+void IUndead::Banish()
+{
+    AActor* Me = Cast<AActor>(this);
+
+    if(Me)
+    {
+        Me->Destroy();
+    }
+}
